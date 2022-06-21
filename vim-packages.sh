2314271ceea0a8d7a4da6cfd70830771a1792c4e
lucerion <alexander.skachko@gmail.com>
2022-06-22 01:52:13 +0300

    Improve vim-packages and pathogen installation

--- vim-packages.sh
+++ vim-packages.sh
@@ -3,6 +3,8 @@
 mkdir -p ~/.vim/pack/vim-as-a-ruby-ide/start
 cd ~/.vim/pack/vim-as-a-ruby-ide/start
 
+curl -o plugins.sh https://raw.githubusercontent.com/lucerion/vim-as-a-ruby-ide/master/plugins.sh
+chmod 766 ./plugins.sh
 . ./plugins.sh
 
 cd -
